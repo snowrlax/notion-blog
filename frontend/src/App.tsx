0b92c96d81648dff0ebf415b5205a80862f95c4f
Snowrlax <sonawanepranav19@gmail.com>
2024-03-21 01:56:22 +0530

    removed unused Link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { Link, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
